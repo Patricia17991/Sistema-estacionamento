7c273f5797eb6f0bc888454adbe101430c6aefb9
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-03-11 14:51:34 -0300

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,10 +1,12 @@
 //vamos de inicioa executar uma função anônima
 (function () {
-    const $ = (query: string) => document.querySelector(query);
+    const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
 
 
-    $("#cadastrar")?.addEventListener("click")//pegando o id cadastrar
+    $("#cadastrar")?.addEventListener("click", () => {
+        //quando clicar em cadastrar
+        
+    })//pegando o id cadastrar
 })()
-
 
 //essa ? noeventlistener o vscode colocou pq o elemento pode vir nulo
